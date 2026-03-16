3947e3c29692be0aa434a32de16a811d1b6a0458
Varunappalla1993 <an.varun1993@gmail.com>
2026-03-16 11:28:49 +0530

    DB

---
+++ 40-databases/r53.tf
@@ -0,0 +1,20 @@
+# Day 40
+
+resource "aws_route53_record" "mongodb" {
+  zone_id         = var.zone_id
+  name            = "mongodb-${var.env}.${var.domain_name}"
+  type            = "A"
+  ttl             = "1"
+  records         = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
+}
+
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.env}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -8,3 +8,13 @@ variable "project" {
 variable "env" {
   default = "dev"
 }
+
+
+variable "zone_id" {
+
+  default = "Z07749522UFFTP3DDWVZQ"
+}
+
+variable "domain_name" {
+  default = "vardevops.online"
+}
