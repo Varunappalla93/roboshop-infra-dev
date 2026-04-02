61fdb2a3f10a9a51222e0b2cb5535a818fe043d1
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-02 16:29:13 +0530

    sg

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -8,7 +8,7 @@
 # }
 
 
-resource "aws_ssm_parameter" "_sg_id" {
+resource "aws_ssm_parameter" "sg_id" {
   count = length(var.sg_names)
   name  = "/${var.project}/${var.env}/${var.sg_names[count.index]}_sg_id"
   type  = "String"
