96d27ec99c15132b650d46773e21180eb0822ee6
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-03 09:26:10 +0530

    main

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -4,5 +4,5 @@ module "vpc" {
   source      = "git::https://github.com/Varunappalla93/Terrform-AWS-VPC.git?ref=main"
   project     = var.project
   environment = var.env
-  is_peering_required=true
+  is_peering_required=false
 }
