a1a1b4adc61eaec2cb2c8bfca26f30de56538fa3
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-04 19:46:05 +0530

    main

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -6,3 +6,7 @@ module "vpc" {
   environment = var.env
   is_peering_required=false
 }
+
+
+# to run all at once
+# for i in 00-vpc/ 10-sg/ 20-sg-rules/ 30-bastion/; do cd $i; terraform apply -auto-approve; cd ..;done
