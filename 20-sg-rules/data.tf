66004aa6c1dd8fc4a15d72ca4e6b49290fd30d99
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-05 15:30:04 +0530

    DB

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -30,3 +30,8 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
 data "aws_ssm_parameter" "user_sg_id" {
   name = "/${var.project}/${var.env}/user_sg_id"
 }
+
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project}/${var.env}/mysql_sg_id"
+}
