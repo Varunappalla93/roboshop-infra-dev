4be142444bc78aed56c8722d6bd96d479e241c59
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-06 14:41:15 +0530

    DB

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -35,3 +35,8 @@ data "aws_ssm_parameter" "user_sg_id" {
 data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project}/${var.env}/mysql_sg_id"
 }
+
+
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project}/${var.env}/rabbitmq_sg_id"
+}
