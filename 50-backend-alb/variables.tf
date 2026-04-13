3f6d936bdd595e3ff41f22729e97d3f4891584e3
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-13 12:12:39 +0530

    backend ABL

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07749522UFFTP3DDWVZQ"
+}
+
+variable "domain_name" {
+    default = "vardevops.online"
+}
