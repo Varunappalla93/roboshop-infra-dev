7972ecb572a0f5dcfe70beedf8fd405b352cdf0e
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-14 10:54:06 +0530

    catalogue

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,21 @@
+# Day 43
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07749522UFFTP3DDWVZQ"
+}
+
+variable "domain_name" {
+    default = "vardevops.online"
+}
+
+variable "app_version" {
+    default = "v3"
+}
