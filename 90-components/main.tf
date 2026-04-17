90d8e7836524322b919ad21acc8ff6c9bb21cfd7
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-17 11:43:26 +0530

    cm

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -2,7 +2,8 @@
 
 module "component" {
     for_each = var.components
-    source = "git::https://github.com/daws-88s/terraform-roboshop-component.git?ref=main"
+    source = "git::https://github.com/Varunappalla93/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
+
