92b4e6f30a8968b31a618ff66dad6502241748d9
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-17 11:36:48 +0530

    components

---
+++ 90-components/main.tf
@@ -0,0 +1,8 @@
+# Day 46
+
+module "component" {
+    for_each = var.components
+    source = "git::https://github.com/daws-88s/terraform-roboshop-component.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
+}
---
+++ 90-components/provider.tf
@@ -0,0 +1,22 @@
+# Day 46
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.33.0" # Terraform AWS provider version
+    }
+  }
+
+  backend "s3" {
+    bucket       = "remote-state-dev-var" # bucket must be created in aws s3 before running terraform init
+    key          = "roboshop-dev-components"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
