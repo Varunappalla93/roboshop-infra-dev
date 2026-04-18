380ddc5e079cf4b7f5bb90eb771028a0727c219e
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-18 10:51:53 +0530

    CDN

---
+++ 95-cdn/data.tf
@@ -0,0 +1,13 @@
+# Day 47
+
+data "aws_cloudfront_cache_policy" "cachingDisabled" {
+  name = "Managed-CachingDisabled"
+}
+
+data "aws_cloudfront_cache_policy" "cachingOptimized" {
+  name = "Managed-CachingOptimized"
+}
+
+data "aws_ssm_parameter" "acm_certificate_arn" {
+  name = "/${var.project}/${var.env}/frontend_alb_certificate_arn"
+}
---
+++ 95-cdn/variables.tf
@@ -0,0 +1,17 @@
+# Day 47
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "env" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z07749522UFFTP3DDWVZQ"
+}
+
+variable "domain_name" {
+    default = "vardevops.online"
+}
